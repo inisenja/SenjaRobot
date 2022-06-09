0c994bd87795ca0e18cf7e2f465394a4a8b6bebf
inisenja <101322982+inisenja@users.noreply.github.com>
2022-06-09 22:28:23 +0700

    Update userinfo.py

--- YinsRobot/modules/userinfo.py
+++ YinsRobot/modules/userinfo.py
@@ -445,9 +445,9 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 Current Yins Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>╔═━「 Current Senja Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By AyiinXd 」</b>"
+    result += "\n<b>╘═━「 Powered By sᴇɴᴊᴀ-ᴇx 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 

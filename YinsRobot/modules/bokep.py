19cb59427aba798c7b663db70a0b0cf2f361346e
inisenja <101322982+inisenja@users.noreply.github.com>
2022-06-11 20:03:26 +0700

    Create bokep.py

---
+++ YinsRobot/modules/bokep.py
@@ -0,0 +1,55 @@
+# 🥂 © @inisenja
+
+# ⚠️ Don't Remove Credits
+
+import os
+
+import random
+
+from telethon.tl.types import InputMessagesFilterPhotos
+
+from telethon.tl.types import InputMessagesFilterVideo
+
+from YinsRobot.events import register
+
+from YinsRobot import telethn as tbot, ubot2                 
+
+@register(pattern="^/bokep ?(.*)")
+
+async def _(event):
+
+    memeks = await event.reply("**Mencari Video Bokep...🔍**") 
+
+    try:
+
+        bokepnya = [
+
+            bokep
+
+            async for bokep in ubot2.iter_messages(
+
+            "@gagaltaubattxixixi", filter=InputMessagesFilterVideo
+
+            )
+
+        ]
+
+        kontols = random.choice(bokepnya)
+
+        pantek = await ubot2.download_media(kontols)
+
+        await tbot.send_file(
+
+            event.chat.id, 
+
+            caption="Jangan lupa coli kak 🥵", 
+
+            file=pantek
+
+            )
+
+        await memeks.delete()
+
+    except Exception:
+
+        await memeks.edit("Bokepnya gaada komsol")

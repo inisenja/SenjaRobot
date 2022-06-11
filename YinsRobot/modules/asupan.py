3da787a2844a22b98749fa1218f6ec97707dc60e
inisenja <101322982+inisenja@users.noreply.github.com>
2022-06-11 08:22:14 +0700

    Update asupan.py

--- YinsRobot/modules/asupan.py
+++ YinsRobot/modules/asupan.py
@@ -56,60 +56,99 @@ async def _(event):
 
 
 @register(pattern="^/ayang ?(.*)")
+
 async def _(event):
+
     try:
         ayangnya = [
+
             ayang
+
             async for ayang in event.client.iter_messages(
+
                 "@CeweLogoPack", filter=InputMessagesFilterPhotos
+
             )
+
         ]
         aing = await event.client.get_me()
+
         await event.client.send_file(
+
             event.chat_id,
+
             file=random.choice(ayangnya),
+
             caption=f"**Ayang nya** [{owner}](tg://user?id={aing.id})",
         )
         await event.delete()
+
     except Exception:
+
         await event.edit("**GA ADA YANG MAU SAMA LO, MAKANYA CAKEP!.**")
 
 
 @register(pattern="^/nc ?(.*)")
+
 async def _(event):
+
     try:
+
         bokepnya = [
+
             bokep
+
             async for bokep in event.client.iter_messages(
+
                 "@fakyudurov", filter=InputMessagesFilterVideo
             )
-        ]
+        
         aing = await event.client.get_me()
+
         await event.client.send_file(
+
             event.chat_id,
+
             file=random.choice(bokepnya),
+
             caption=f"**negative content by** [{owner}](tg://user?id={aing.id})",
         )
         await event.delete()
+
     except Exception:
+
         await event.edit("**tidak ditemukan, tahsn dulu sangenya.**")
 
 
 @register(pattern="^/ncp ?(.*)")
+
 async def _(event):
+
     try:
+
         kontenrexnya = [
+
             kontenrex
+
             async for kontenrex in event.client.iter_messages(
+
                 "@durovbgst", filter=InputMessagesFilterPhotos
+
             )
+
         ]
         xa = await event.client.get_me()
+
         await event.client.send_file(
+
             event.chat_id,
+
             file=random.choice(kontenrexnya),
+
             caption=f"adult photos by [{owner}](tg://user?id={xa.id})",
         )
         await event.delete()
+
     except Exception:
+
         await event.edit("**tidak ditemukan. **")

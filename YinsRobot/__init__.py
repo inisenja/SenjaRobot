eb4a3fb4752989cbc8f093130a3a0e206b665551
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-06-28 19:29:03 +0700

    Update __init__.py

--- YinsRobot/__init__.py
+++ YinsRobot/__init__.py
@@ -23,7 +23,7 @@ from ptbcontrib.postgres_persistence import PostgresPersistence
 StartTime = time.time()
 
 def get_user_list(__init__, key):
-    with open("{}/SenjaRobot/{}".format(os.getcwd(), __init__), "r") as json_file:
+    with open("{}/YinsRobot/{}".format(os.getcwd(), __init__), "r") as json_file:
         return json.load(json_file)[key]
 
 # enable logging

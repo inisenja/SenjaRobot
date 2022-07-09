c6d99f6d6e15457f45c8504de24224ce7875c670
inisenja <101322982+inisenja@users.noreply.github.com>
2022-07-10 06:40:26 +0700

    Update alive.py

--- YinsRobot/modules/alive.py
+++ YinsRobot/modules/alive.py
@@ -9,17 +9,17 @@ from YinsRobot.events import register
 from YinsRobot import telethn as tbot
 
 yinzver = "2.0.22"
-PHOTO = "https://telegra.ph/file/fc8bdd4dc22db9768de00.jpg"
+PHOTO = "https://telegra.ph/file/c05b3fd99bb2cf3eaa270.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'ᴍ Bᴀʙʏʟᴀ Mᴜsɪᴄ Bᴏᴛ.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'ᴍ Bᴀʙʏʟᴀ Rᴏʙᴏᴛ.** \n\n"
   TEXT += "㋭ **I'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ** \n\n"
-  TEXT += f"㋭ **ᴏᴡɴᴇʀ : [sᴇɴᴊᴀ-ᴇx](https://t.me/senja_ex)** \n\n"
+  TEXT += f"㋭ **ᴏᴡɴᴇʀ : [ʙᴀʙʏʟᴀ](https://t.me/xbbyla)** \n\n"
   TEXT += f"㋭ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ    :** `{telever}` \n\n"
   TEXT += f"㋭ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
   TEXT += f"㋭ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += f"㋭ **ʙᴀʙʏʟᴀᴍᴜsɪᴄʙᴏᴛ ᴠᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
+  TEXT += f"㋭ **ʙᴀʙʏʟᴀʀᴏʙᴏᴛ ᴠᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
   TEXT += "**Thanks For Adding Me Here ㋭**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Babylamusicbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/senjaasupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
